4d2494ea37a3500925c50106e63fa87e2dcb26e9
erkitammeaid <erkipoiss01@gmail.com>
2021-01-14 15:34:39 +0200

    suicide

--- src/views/UI-001.vue
+++ src/views/UI-001.vue
@@ -1,8 +1,70 @@
 <template>
   <div class="flex-center">
     <div class="home-content">
-      <h1>Siia tuleb esimene ülesanne (sign-up form)</h1>
-      <p>Challenge: Design Hint... Create a sign up page, modal, form, or app screen related to signing up for something. It could be for a volunteer event, contest registration, a giveaway, or anything you can image.</p>
+      <div class="signupcontent">
+        <h1>Sign up form</h1>
+        <div class="usern">
+            Kasutajanimi
+          <input name="username" placeholder="Sisestage kasutajanimi" />
+        </div>
+        <div class="pw">
+          Parool
+          <input
+            name="password"
+            placeholder="Sisestage parool"
+            type="password"/>
+        </div>
+        <div class="signup">
+          <button class="signupbtn">Sign up</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style lang="scss">
+.flex-center {
+  display: flex;
+  justify-content: center;
+}
+.home {
+  &-content {
+    max-width: 50vh;
+    min-width: 400px;
+  }
+}
+.signupcontent {
+  background: lightsteelblue;
+}
+.pw {
+  display: flex;
+  flex-direction: column;
+  margin-left: 7.5rem;
+  justify-content: center;
+  padding-top: 1rem;
+  width: 10rem;
+}
+.usern {
+  display: flex;
+  flex-direction: column;
+  padding-top: 1rem;
+  justify-content: center;
+   margin-left: 7.5rem;
+   width: 10rem;
+}
+.signup {
+  padding: 1.3rem;
+.signupbtn {
+    background: greenyellow;
+    border: black;
+}
+}
+</style>
--- src/views/UI-002.vue
+++ src/views/UI-002.vue
@@ -1,12 +1,49 @@
 <template>
-  <div class="flex-center">
-    <div class="home-content">
-      <h1>Siia tuleb teine ülesanne (sign-up form)</h1>
-      <p>
-        Challenge: Design Hint... Create a sign up page, modal, form, or app
-        screen related to signing up for something. It could be for a volunteer
-        event, contest registration, a giveaway, or anything you can image.
-      </p>
+  <body>
+    <div class="container">
+      <div class="pede">
+        <h1 id="text">Loading Please Wait</h1>
+      </div>
+      <div class="loader"></div>
     </div>
-  </div>
+  </body>
 </template>
+<style>
+.pede {
+  display: flex;
+  height: 8rem;
+  align-items: center;
+}
+body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+.loader {
+  display: flex;
+  justify-content: center;
+  margin: 10px;
+  border: 16px solid #f3f3f3; /* Light grey */
+  border-top: 16px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
